3102a8aeab76ba885cfc0a0a01febca38c7d3c03
Thalia <97253671+userThaliaoliveira@users.noreply.github.com>
2023-12-06 17:02:36 -0300

    Update Pedido.java

--- Quebra-tudo-V2/src/principal/Pedido.java
+++ Quebra-tudo-V2/src/principal/Pedido.java
@@ -6,16 +6,19 @@ public class Pedido {
     int quantidade;
     double preco;
 
+    // Construtor para inicializar um Pedido com nome do produto, quantidade e preço
     public Pedido(String nomeProduto, int quantidade, double preco) {
         this.nomeProduto = nomeProduto;
         this.quantidade = quantidade;
         this.preco = preco;
     }
 
+    // Método para calcular o valor total do pedido (quantidade * preço)
     public double calcularValorTotal() {
         return quantidade * preco;
     }
 
+    // Sobrescrita do método toString para exibir informações do Pedido
     @Override
     public String toString() {
         return "Produto: " + nomeProduto + ", Quantidade: " + quantidade + ", Preço: " + preco +

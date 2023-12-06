5efc3e1cdd4641624e75affb03ed974dd9f804fe
Thalia <97253671+userThaliaoliveira@users.noreply.github.com>
2023-12-06 17:02:03 -0300

    Update Cliente.java

--- Quebra-tudo-V2/src/principal/Cliente.java
+++ Quebra-tudo-V2/src/principal/Cliente.java
@@ -6,16 +6,19 @@ public class Cliente {
     String cpf;
     Pedido pedido;
 
+    // Construtor padrão vazio
     public Cliente() {
 
     }
 
+    // Construtor com parâmetros para inicializar um Cliente com nome, CPF e Pedido
     public Cliente(String nome, String cpf, Pedido pedido) {
         this.nome = nome;
         this.cpf = cpf;
         this.pedido = pedido;
     }
 
+    // Sobrescrita do método toString para exibir informações do Cliente e seu Pedido
     @Override
     public String toString() {
         return "Nome: " + nome + ", CPF: " + cpf + ", Pedido: " + pedido.toString() +
